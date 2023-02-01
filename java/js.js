459b5c4a7431841dc88a1a371af2e1b79624eed6
DW <dnldp44@naver.com>
2023-02-01 16:13:38 +0900

    🔒️ 네이버폼 complete

--- java/js.js
+++ java/js.js
@@ -11,6 +11,10 @@ $("input").focusout(function() {
 
 //아이디
 //#userid input에서 focusout 됐을 때 그것의 글자수가 0이라면 (조건)
+// #userid .warn에 '필수 정보입니다.'라고 쓴다 (실행문1)
+
+// len이 5보다 작거나 20보다 클 때 (조건2)  - 논리연산자 사용 or ||
+// #userid .warn에 '5~20자의 영소문자만 사용 가능합니다.'라고 쓴다 (실행문1)
 $("#userid input").focusout(function() {
   let len = $(this).val().length;
   idveri = false;
@@ -25,7 +29,8 @@ $("#userid input").focusout(function() {
 })
 
 //비밀번호
-//
+// #userpw input에서 focusout 됐을 때 그것의 글자수가 0이라면 (조건1)
+// #userpw .warn에 '필수 정보입니다.'라고 쓴다 (실행문1) .text-red 부여
 $("#userpw input").focusout(function() {
   let len = $(this).val().length;
   pwveri = false;
@@ -52,8 +57,19 @@ $("#userpw input").focusout(function() {
 
 
 
-//비밀번호 재확인
-//userpwchk input에서 focusout 됐을 때  value 값이
+// 비밀번호 재확인
+// #userpw-chk input 에서 focusout 됐을 때 value값의 length가 0이라면 (조건1)
+// #userpw-chk .warn에 빨간 글씨로 "필수 정보입니다" (실행문1)
+// #userpw-chk .inputbox img의 속성중에 src 경로 값을 변경 (실행문2)
+
+// 그렇지 않다면 그것의 값과 #userpw input 안에 들어있는 값이 같은가? (조건2)
+// pwchkveri 변수를 true로 변경 (실행문1)
+// #userpw-chk .warn에 empty()로 비운다. (실행문2)
+// #userpw-chk .inputbox img의 속성중에 src 값을 변경한다.->images/m_icon_pw_step_07.png
+
+// 그렇지 않다면 else
+// #userpw-chk .warn '비밀번호가 일치하지 않습니다.' (실행문1)
+// #userpw-chk .inputbox img src -> images/m_icon_pw_step_02.png 변경
 
 $("#userpw-chk input").focusout(function() {
   let userpwchk = $(this).val();
